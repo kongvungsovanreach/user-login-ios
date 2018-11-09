77e5f6a1b45a7914c2fdd0dbe0b2ac6920fe3eef
Kong vungsovanreach <vungsovanreachkong@gmail.com>
2018-11-09 11:12:52 +0700

    update

--- LoginHomework/ViewControllers/LoginPageViewController.swift
+++ LoginHomework/ViewControllers/LoginPageViewController.swift
@@ -10,30 +10,44 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
 
-    @IBOutlet weak var usernameTextView: UITextField!
-    @IBOutlet weak var passwordTextView: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextFiled: UITextField!
 
     @IBAction func signupButtonTap(_ sender: Any) {
+        print(UserSampleData.users.count)
         self.performSegue(withIdentifier: "loginToSignup", sender: self)
     }
 
     @IBAction func loginButtonTap(_ sender: Any) {
-    
+        let username = usernameTextField.text!
+        let password = passwordTextFiled.text!
+        let users = UserSampleData.users
+        for user in users {
+            if user.username == username && user.password == password {
+                print("login successfully")
+            }
+        }
     }
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         textViewConfig()
+        buttonConfig()
 //        usernameTextView.scroll
 
         // Do any additional setup after loading the view.
     }
     func textViewConfig() {
-        usernameTextView.placeholder = "Input Your Username!"
-        usernameTextView.frame.size.height = 60
-        passwordTextView.placeholder = "Input Your Password!"
-        passwordTextView.frame.size.height = 60
+        usernameTextField.placeholder = "Input Your Username!"
+        usernameTextField.frame.size.height = 60
+        passwordTextFiled.placeholder = "Input Your Password!"
+        passwordTextFiled.frame.size.height = 60
+    }
+    func buttonConfig() {
+        loginButton.layer.cornerRadius = 7
+        signupButton.layer.cornerRadius = 7
     }
     
 
--- LoginHomework/ViewControllers/SignupPageViewController.swift
+++ LoginHomework/ViewControllers/SignupPageViewController.swift
@@ -9,20 +9,64 @@
 import UIKit
 
 class SignupPageViewController: UIViewController {
-    @IBOutlet weak var usernameTextView: UITextField!
-    @IBOutlet weak var passwordTextView: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-textViewConfig()
-        // Do any additional setup after loading the view.
-    }
-    func textViewConfig() {
-        usernameTextView.placeholder = "Input Your Username!"
-        usernameTextView.frame.size.height = 60
-        passwordTextView.placeholder = "Input Your Password!"
-        passwordTextView.frame.size.height = 60
+
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBAction func goBackButtonTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func signupButtonTap(_ sender: Any) {
+        let username = usernameTextField.text!
+        let password = passwordTextField.text!
+        let confirmPassword = confirmPasswordTextField.text!
+        if username.isEmpty || password.isEmpty || confirmPassword.isEmpty {
+            giveAlert(info: "Please input all fields!", bottomTitle: "OK, I got it.", handler: false)
+        }else if !confirmPasswordCheck() {
+            giveAlert(info: "Confirm password is not match!", bottomTitle: "I'll check it again.", handler: false)
+        }else{
+            var  newUser = User(username: usernameTextField.text!, password: passwordTextField.text!)
+            UserSampleData.users.append(newUser)
+            giveAlert(info: "Signup successfully!", bottomTitle: "Login Now!", handler: true)
+
+        }
+
+
+
+
+//
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textViewConfig()
+        buttonConfig()
+        // Do any additional setup after loading the view.
+    }
+    func giveAlert(info : String, bottomTitle : String, handler : Bool) {
+        let alert = UIAlertController(title: info, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: bottomTitle, style: .cancel, handler: handler ? {(alert) -> Void in self.dismiss(animated: true, completion: nil)
+            }: {(alert) -> Void in print("No handler")}
+        ))
+        self.present(alert, animated: true)
+    }
+
+    func confirmPasswordCheck() -> Bool {
+        return passwordTextField.text! == confirmPasswordTextField.text ? true : false
+    }
+
+
+    func textViewConfig() {
+        usernameTextField.frame.size.height = 60
+        passwordTextField.frame.size.height = 60
+        confirmPasswordTextField.frame.size.height = 60
+    }
+    func buttonConfig() {
+        signupButton.layer.cornerRadius = 7
+    }
     /*
     // MARK: - Navigation
 
@@ -33,4 +77,14 @@ textViewConfig()
     }
     */
 
+}
+extension UITextField{
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: newValue!])
+        }
+    }
 }
