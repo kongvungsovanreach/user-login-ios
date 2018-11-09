7363caa13de0ad83d1b3b0fdfc666bd3c2469399
Kong vungsovanreach <vungsovanreachkong@gmail.com>
2018-11-09 16:42:15 +0700

    Update

--- LoginHomework/ViewControllers/ViewController.swift
+++ LoginHomework/ViewControllers/ViewController.swift
@@ -10,22 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     var user : User!
-
+    var isLogin = false
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     @IBAction func loginButtonTap(_ sender: Any) {
-        self.performSegue(withIdentifier: "mainToLoginPage", sender: self)
+        if isLogin {
+            user = nil
+            viewDidLoad()
+        }else{
+            self.performSegue(withIdentifier: "mainToLoginPage", sender: self)
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         styleButton()
         if user == nil {
-            print("first login")
+            loginButton.setTitle("LOGIN", for: UIControl.State.normal)
             welcomeLabel.isHidden = true
+            loginButton.backgroundColor = UIColor.orange
+            isLogin = false
         }else{
             welcomeLabel.text = "Welcome \(user.username)"
+            loginButton.setTitle("LOGOUT", for: UIControl.State.normal)
             loginButton.backgroundColor = UIColor.green
+            isLogin = true
         }
 
     }
@@ -34,6 +43,7 @@ class ViewController: UIViewController {
         loginButton.frame = CGRect(x: 125, y: 500, width: 170, height: 170)
         loginButton.layer.cornerRadius = 0.5 * loginButton.bounds.size.width
         loginButton.clipsToBounds = true
+    
     }
 }
 
